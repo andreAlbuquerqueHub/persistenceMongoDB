894cd0663b6a914e3738db741e91d384402e1838
Daniel Kchevi <dkchevi@hotmail.com>
2020-08-12 22:45:19 -0300

    estrutura base api loja

---
+++ loja-mongo/src/main/java/com/fiap/lojamongo/LojaMongoApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.lojamongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaMongoApplication.class, args);
+	}
+
+}
---
+++ loja-mongo/src/test/java/com/fiap/lojamongo/LojaMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.lojamongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
